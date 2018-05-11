9290e45d07d763cc075294c1335e7bd2d6cc6af8
mesargent <markesargent@gmail.com>
2018-05-10 19:21:15 -0700

    adding classes to start refactoring

---
+++ detector.py
@@ -0,0 +1,12 @@
+class Detector:
+	def __init__(self):
+		self.preprocessors = []
+		self.region_generator = None
+		self.classifier = None
+
+	def detect_photo(self, photo):
+		pass
+
+	def detect_video(self, video):
+		pass
+
---
+++ region_finder.py
@@ -0,0 +1,6 @@
+class RegionFinder:
+	def __init__(self):
+		pass
+
+	def find_regions(self, image):
+		pass
