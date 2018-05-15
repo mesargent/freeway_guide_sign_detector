cd8e0d8006324680c25110153a854d07204be6a1
mesargent <markesargent@gmail.com>
2018-05-15 11:59:30 -0700

    added config.yml, main program (fgsd.py)

---
+++ fgsd.py
@@ -0,0 +1,22 @@
+import yaml
+import sys
+
+
+def main():
+	with open("config.yml", "r") as yaml_file:
+		config = yaml.load(yaml_file)
+
+	def detect_signs_in_images():
+		print("detecting signs in images folder {}/".format(config['image_info']['image_paths']['detect']))
+
+	def detect_signs_in_video():
+		print("detecting signs in video")
+
+	dispatch = {'detect_images': detect_signs_in_images, 'detect_video': detect_signs_in_video}
+	
+	command = sys.argv[1]
+	dispatch[command]()
+
+
+if __name__ == '__main__':
+	main()
